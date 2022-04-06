23343c8e2b8a479e528d99357032a9d50ea9a9ec
Anderson <andersonst.dev@gmail.com>
2022-04-05 22:05:59 -0300

    create component of product render and error message

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,17 @@ import { useFetchProducts } from '../hooks/use-fetch-products';
 import ProductCard from '../components/product-card';
 import Search from '../components/search';
 
+const ProductNode = ({ products = [] }) => {
+  if (!products.length) return <h4 data-testid="no-products">No Products</h4>;
+  return products.map((product) => (
+    <ProductCard key={product.id} product={product} />
+  ));
+};
+
+const ErrorMessage = () => {
+  return <h4 data-testid="server-error">Server is down</h4>;
+};
+
 export default function Home() {
   const { products, error } = useFetchProducts();
 
@@ -12,9 +23,8 @@ export default function Home() {
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
+          {error && <ErrorMessage error={error} />}
+          {!error && <ProductNode products={products} />}
         </div>
       </div>
     </main>
