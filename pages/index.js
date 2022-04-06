efdc7335b1011c6124e055037e46769b6aa87bad
Anderson <andersonst.dev@gmail.com>
2022-04-05 22:22:30 -0300

    create feature to filter products list by search term

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useFetchProducts } from '../hooks/use-fetch-products';
 import ProductCard from '../components/product-card';
 import Search from '../components/search';
@@ -15,16 +16,29 @@ const ErrorMessage = () => {
 
 export default function Home() {
   const { products, error } = useFetchProducts();
+  const [term, setTerm] = useState('');
+  const [localProducts, setLocalProducts] = useState([]);
+
+  function doSearch() {}
+
+  useEffect(() => {
+    if (term === '') setLocalProducts(products);
+    setLocalProducts(
+      products.filter(({ title }) => {
+        return title.toLowerCase().includes(term.toLowerCase());
+      }),
+    );
+  }, [products, term]);
 
   return (
     <main data-testid="product-list" className="my-8">
-      <Search />
+      <Search doSearch={setTerm} />
       <div className="container mx-auto px-6">
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
           {error && <ErrorMessage error={error} />}
-          {!error && <ProductNode products={products} />}
+          {!error && <ProductNode products={localProducts} />}
         </div>
       </div>
     </main>
