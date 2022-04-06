e0a34635b8e628fc8175cb1ca491f4e2dbc2aa2a
Anderson <andersonst.dev@gmail.com>
2022-04-05 22:05:41 -0300

    test when server is down

--- pages-tests/product-list.integration.spec.js
+++ pages-tests/product-list.integration.spec.js
@@ -1,6 +1,7 @@
 import { screen, render, waitFor } from '@testing-library/react';
 import ProductList from '../pages';
 import { makeServer } from '../miragejs/server';
+import Response from 'miragejs';
 
 const renderProductList = () => {
   render(<ProductList />);
@@ -30,6 +31,28 @@ describe('ProductList', () => {
 
     await waitFor(() => {
       expect(screen.getAllByTestId('product-card')).toHaveLength(10);
+    });
+  });
+
+  it('should render the "no products message"', async () => {
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('no-products')).toBeInTheDocument();
+    });
+  });
+
+  it('should display error message when promise rejects', async () => {
+    server.get('products', () => {
+      return new Response(500, {}, '');
+    });
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('server-error')).toBeInTheDocument();
+      expect(screen.queryByTestId('no-products')).toBeNull();
+      expect(screen.queryAllByTestId('product-card')).toHaveLength(0);
     });
   });
 });
