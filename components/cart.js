94496c052dc74f3254f5c1b83c1d78e376afb6fd
Anderson <andersonst.dev@gmail.com>
2022-04-09 01:01:42 -0300

    show clear cart button only when has products in the cart

--- components/cart.js
+++ components/cart.js
@@ -16,7 +16,7 @@ export default function Cart() {
     >
       <div className="flex items-center justify-between">
         <h3 className="text-2xl font-medium">Your cart</h3>
-        <button onClick={removeAll}>Clear cart</button>
+        {hasProducts && <button onClick={removeAll}>Clear cart</button>}
         <button
           data-testid="close-button"
           onClick={toggle}
